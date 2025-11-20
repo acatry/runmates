9e388ecc306e0071bd9a7447d36e676c01c2404e
Anthony <anthony.catry@gmail.com>
2025-11-20 10:59:45 +0100

    Création de Gates pour réserver certaines routes uniquement à certains rôles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Providers;
+use App\Models\Event;
+use App\Models\RunningSession;
+use Illuminate\Support\Facades\Gate;
+
 
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +23,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('event-create', function ($user) {
+            return $user->isOrganizer();
+        });
+
+        Gate::define('event-update', function ($user, Event $event) {
+            return $user->isOrganizer() && $user->id === $event->organizer_id;
+        });
+
+        Gate::define('event-delete', function ($user, Event $event) {
+            return $user->isOrganizer() && $user->id === $event->organizer_id;
+        });
+
+        Gate::define('session-create', function ($user) {
+            return $user->isSporty();
+        });
+
+        Gate::define('session-update', function ($user, RunningSession $session) {
+            return $user->isSporty() && $user->id === $session->organizer_id;
+        });
+
+        Gate::define('session-delete', function ($user, RunningSession $session) {
+            return $user->isSporty() && $user->id === $session->organizer_id;
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ use \App\Http\Controllers\CommentController;
 use App\Http\Controllers\EventVolunteerController;
 use App\Http\Controllers\NotificationController;
 
-
-
-
-
 Route::get('/', function () {
     if (auth()->check()) {
         return redirect()->route('events.index'); 
@@ -27,60 +23,48 @@ Route::get('/', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])
         ->name('dashboard');
-    //EVENTS
-    Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-    Route::post('/events', [EventController::class, 'store'])->name('events.store');
+    //EVENTS + GATE LA OU C'EST NECESSAIRE
+    Route::get('/events/create', [EventController::class, 'create'])->name('events.create')->can('event-create');
+    Route::post('/events', [EventController::class, 'store'])->name('events.store')->can('event-create');
     Route::get('/events', [EventController::class, 'index'])->name('events.index');
     Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
     Route::post('/events/{event}/register', [EventRegistrationController::class, 'store'])
         ->name('events.register');
     Route::delete('/events/{event}/register', [EventRegistrationController::class, 'destroy'])
         ->name('events.unregister');
-    //EDIT + DELETE
-    Route::get('/events/{event}/edit', [EventController::class, 'edit'])
-        ->name('events.edit');
-    Route::put('/events/{event}', [EventController::class, 'update'])
-        ->name('events.update');
-    Route::delete('/events/{event}', [EventController::class, 'delete'])
-    ->name('events.delete');
+    //EDIT + DELETE + GATE
+    Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit')->can('event-update', 'event');
+    Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update')->can('event-update', 'event');
+    Route::delete('/events/{event}', [EventController::class, 'delete'])->name('events.delete')->can('event-delete', 'event');
 
-    //RUNNING SESSION
-    Route::get('/running-sessions', [RunningSessionController::class, 'index'])->name('running-sessions.index');
-    Route::get('/running-sessions/create', [RunningSessionController::class, 'create'])->name('running-sessions.create');
+    //RUNNING SESSION + GATE LA OU C'EST NECESSAIRE
+    Route::get('/running-sessions', [RunningSessionController::class, 'index'])->name('running-sessions.index')->can('session-create');
+    Route::get('/running-sessions/create', [RunningSessionController::class, 'create'])->name('running-sessions.create')->can('session-create');
     Route::post('/running-sessions', [RunningSessionController::class, 'store'])->name('running-sessions.store');
     Route::get('/running-sessions/{runningSession}', [RunningSessionController::class, 'show'])->name('running-sessions.show');
-    Route::post('/running-sessions/{runningSession}/join', [RunningSessionParticipationController::class, 'store'])
-        ->name('running-sessions.join');
-    Route::delete('/running-sessions/{runningSession}/join', [RunningSessionParticipationController::class, 'destroy'])
-        ->name('running-sessions.leave');
-    //EDIT + DELETE
-    Route::get('/running-sessions/{runningSession}/edit', [RunningSessionController::class, 'edit'])
-        ->name('running-sessions.edit');
-    Route::put('/running-sessions/{runningSession}', [RunningSessionController::class, 'update'])
-        ->name('running-sessions.update');
-    Route::delete('/running-sessions/{runningSession}', [RunningSessionController::class, 'delete'])
-    ->name('running-sessions.delete');
+    Route::post('/running-sessions/{runningSession}/join', [RunningSessionParticipationController::class, 'store'])->name('running-sessions.join');
+    Route::delete('/running-sessions/{runningSession}/join', [RunningSessionParticipationController::class, 'destroy'])->name('running-sessions.leave');
+    //EDIT + DELETE + GATE
+    Route::get('/running-sessions/{runningSession}/edit', [RunningSessionController::class, 'edit'])->name('running-sessions.edit')->can('session-update', 'runningSession');
+    Route::put('/running-sessions/{runningSession}', [RunningSessionController::class, 'update'])->name('running-sessions.update')->can('session-update', 'runningSession');
+    Route::delete('/running-sessions/{runningSession}', [RunningSessionController::class, 'delete'])->name('running-sessions.delete')->can('session-delete', 'runningSession');
 
     //VUE NOTIFICATIONS
-    Route::get('/notifications', [NotificationController::class, 'index'])
-        ->name('notifications.index');
+    Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
 
 
     //PROFILE
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/runner/{user}', [PublicProfileController::class, 'show'])
-    ->name('runner.profile');
+    Route::get('/runner/{user}', [PublicProfileController::class, 'show'])->name('runner.profile');
 
     //COMMENTS
     Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
 
     // BÉNÉVOLES SUR LES ÉVÈNEMENTS
-    Route::post('/events/{event}/volunteers', [EventVolunteerController::class, 'store'])
-        ->name('events.volunteers.store');
-    Route::delete('/events/{event}/volunteers', [EventVolunteerController::class, 'delete'])
-        ->name('events.volunteers.delete');
+    Route::post('/events/{event}/volunteers', [EventVolunteerController::class, 'store'])->name('events.volunteers.store');
+    Route::delete('/events/{event}/volunteers', [EventVolunteerController::class, 'delete'])->name('events.volunteers.delete');
 
 });
 
