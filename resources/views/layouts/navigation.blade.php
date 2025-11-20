f7ff13856410adf0cd71a333de2dd924d5a4dd6b
Anthony <anthony.catry@gmail.com>
2025-11-20 12:44:29 +0100

    Modif au niveau du menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,28 +11,52 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex sm:items-center">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Accueil') }}
                     </x-nav-link>
-                        @if (auth()->user()->isOrganizer())
-                        <x-nav-link :href="route('events.create')" :active="request()->routeIs('events.create')">
-                        {{ __('Créer un événement') }}
-                        @endif
-                    </x-nav-link>
-                        <x-nav-link :href="route('events.index')" :active="request()->routeIs('events.show')">
-                        {{ __('Événements') }}
-                    </x-nav-link>
-                        @if (auth()->user()->isSporty())
-                    <x-nav-link :href="route('running-sessions.index')" :active="request()->routeIs('running-sessions.show')">
-                       {{ __('Sessions d’entraînement') }}
-                       @endif
-                    </x-nav-link>
-                        @if (auth()->user()->isSporty())
-                    <x-nav-link :href="route('running-sessions.create')" :active="request()->routeIs('running-sessions.create')">
-                       {{ __('Publier une annonce') }}
-                       @endif
-                    </x-nav-link>
+                    <x-dropdown align="left">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700">
+                                Événements
+                                <x-icon-chevron-down class="ms-1 w-4 h-4" />
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link href="{{ route('events.index') }}">
+                                Consulter
+                            </x-dropdown-link>
+
+                            @if (auth()->user()->isOrganizer())
+                                <x-dropdown-link href="{{ route('events.create') }}">
+                                    Créer
+                                </x-dropdown-link>
+                            @endif
+                        </x-slot>
+                    </x-dropdown>
+
+                    <x-dropdown align="left">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700">
+                                Sessions d’entraînement
+                                <x-icon-chevron-down class="ms-1 w-4 h-4" />
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link href="{{ route('running-sessions.index') }}">
+                                Consulter
+                            </x-dropdown-link>
+
+                            @if (auth()->user()->isSporty())
+                                <x-dropdown-link href="{{ route('running-sessions.create') }}">
+                                    Créer
+                                </x-dropdown-link>
+                            @endif
+                        </x-slot>
+                    </x-dropdown>
+
                     <x-nav-link 
                         :href="route('runner.profile', auth()->id())" :active="request()->routeIs('runner.profile')">
                         {{ __('Mon profil') }}
@@ -44,14 +68,9 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
+                        <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700">
+                                 <div>{{ Auth::user()->name }}</div>
+                                <x-icon-chevron-down class="ms-1 w-4 h-4" />
                         </button>
                     </x-slot>
 
